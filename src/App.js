fa6311f35c1656b9fffad817a565157cbd7f0f70
Gilad Madar <gmadar@gmail.com>
2017-12-27 09:52:56 +0200

    created basic UI with fetch implementation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Navbar, NavbarGroup, NavbarHeading, Card, Icon } from "@blueprintjs/core";
 import './App.css';
+import '@blueprintjs/core/dist/blueprint.css';
+
+const cardStyle = {
+  display: "inline-block",
+  width: "20%"
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {loading: false, coins: []}
+    this.getUpdatedData = this.getUpdatedData.bind(this);
+    this.getUpdatedData()
+
+    // Toggle the state every second
+    setInterval(this.getUpdatedData, 30000);
+  }
+  getUpdatedData(){
+    this.setState({ loading: true });
+    fetch('http://localhost:3001/')
+    .then(res => res.json())
+    .then((res) => {
+      this.setState({ loading: false, coins: res });
+    })
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar>
+            <NavbarGroup>
+                <NavbarHeading>Coins</NavbarHeading>
+            </NavbarGroup>
+            <NavbarGroup align="right">
+                {this.state.loading ? 'Loading...' : ''}
+            </NavbarGroup>
+        </Navbar>
+        {this.state.coins.map(c => (
+          <div style={cardStyle}>
+            <Card interactive={true} elevation={Card.ELEVATION_TWO}>
+                <h5><a href="#">{c.name}</a></h5>
+                <p>Rank: <strong>{c.rank}</strong></p>
+                <p>Market Cap: <strong>${c.market_cap_usd || 0}</strong></p>
+                <p>Price: <strong>${c.price_usd || 0}</strong></p>
+                <p>24H Volume: <strong>{c['24h_volume_usd']}</strong></p>
+                <p>Available Supply: <strong>{c.available_supply || 'N/A'}</strong></p>
+                <p>Max Supply: <strong>{c.max_supply || 'N/A'}</strong></p>
+                <p>Weekly Change: <strong>{c.percent_change_7d}%</strong></p>
+            </Card>
+          </div>
+        ))}
       </div>
     );
   }
